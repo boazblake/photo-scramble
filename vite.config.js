a9d9e24e441929536a62cee41232d538c00fecf1
bblake <bblake@goempyrean.com>
2022-12-22 15:14:31 -0600

    pwa

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,41 @@ import path from "path"
 export default defineConfig({
   optimizeDeps: { esbuildOptions: { plugins: [esbuildFlowPlugin()] } },
   plugins: [flowPlugin(),
-  VitePWA({ registerType: 'auto', devOptions: { enabled: false } })],
+  VitePWA({
+    includeAssets: ['favicon.ico',], manifest: {
+      "theme_color": "#f69435",
+      "background_color": "#f69435",
+      "display": "fullscreen",
+      "scope": "/",
+      "start_url": "/",
+      "name": "Photo Scramble",
+      "short_name": "Photo Scrambler",
+      "description": "Photo Scrambler Game",
+      "icons": [
+        {
+          "src": "./assets/images/icon-192x192.png",
+          "sizes": "192x192",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/images/icon-256x256.png",
+          "sizes": "256x256",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/images/icon-384x384.png",
+          "sizes": "384x384",
+          "type": "image/png"
+        },
+        {
+          "src": "./assets/images/icon-512x512.png",
+          "sizes": "512x512",
+          "type": "image/png"
+        }
+      ]
+    }
+    , registerType: 'auto', devOptions: { enabled: false }
+  })],
   resolve: {
     alias: {
       m: "mithril",
