6c6c03e6bd9a206a7b6bc0544b1f7fa07b842569
bblake <bblake@goempyrean.com>
2022-12-28 09:46:57 -0600

    lottie error

--- src/index.js
+++ src/index.js
@@ -105,7 +105,7 @@ const ImageSelector = {
 
 
 const Header = {
-  view: ({ attrs: { mdl } }) => m('section#header',
+  view: ({ attrs: { mdl } }) => m('section#header.col',
     m('code.text.row', { style: { letterSpacing: '3px', fontSize: '2rem' } }, mdl.img.src() && m('', { style: { width: '100px' } }, m(LogoStill)),
       'PHOTO SCRAMBLE!'),
     m(Toolbar, { mdl }),
--- src/logo.js
+++ src/logo.js
@@ -6,7 +6,7 @@ const makeLogo = ({ dom }) => {
   const svgContainer = dom;
   const animItem = bodymovin.loadAnimation({
     wrapper: svgContainer,
-    animType: 'svg',
+    animType: '',
     loop: true,
     path: 'https://lottie.host/d67ca5d8-423c-4cac-96ae-a9b25b89a49c/wE4jiOh8XE.json'
   });
