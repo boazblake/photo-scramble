82663ad13e06882f45bad6468555aa7aae87b9b3
bblake <bblake@goempyrean.com>
2022-12-28 10:03:43 -0600

    lottie error

--- src/logo.js
+++ src/logo.js
@@ -1,14 +1,14 @@
 import m from 'mithril'
 import bodymovin from 'lottie-web'
-
+import { logoLotti } from './files/logo/logo-lottie'
 
 const makeLogo = ({ dom }) => {
-  const svgContainer = dom;
-  const animItem = bodymovin.loadAnimation({
-    wrapper: svgContainer,
+  bodymovin.loadAnimation({
+    wrapper: dom,
     animType: '',
-    loop: true,
-    path: 'https://lottie.host/d67ca5d8-423c-4cac-96ae-a9b25b89a49c/wE4jiOh8XE.json'
+    loop: false,
+    animationData: logoLotti,
+    // path: 'https://lottie.host/d67ca5d8-423c-4cac-96ae-a9b25b89a49c/wE4jiOh8XE.json'
   });
 }
 
